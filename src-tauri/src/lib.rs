ba8d1dc99b1df8ca02383da534b1684f9bc0156e
yofabr <jofab08@gmail.com>
2026-06-10 19:48:26 +0300

    chore: Fix GPU issues

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,18 @@
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let win = app.get_webview_window("main").unwrap();
+            let win2 = win.clone();
+            win.on_window_event(move |event| {
+                if let tauri::WindowEvent::Focused(true) = event {
+                    let _ = win2.eval("window.__sebRepaint = Date.now()");
+                }
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
